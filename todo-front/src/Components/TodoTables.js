95905973b4c1005aa35fc39261c8e49d0b95aff8
Isa Hänninen <55230101+IsaEmilia@users.noreply.github.com>
2022-03-09 15:12:05 +0200

    Small changes to front.

--- todo-front/src/Components/TodoTables.js
+++ todo-front/src/Components/TodoTables.js
@@ -6,13 +6,24 @@ export const TodoTables = ({ todoTable }) => {
     return (
         <div>
             <p>tässä pitäisi olla todo kaavio :)</p>
+
             {todoTable.map(todo => {
                 return (
-                    <ul key={todo.id}>
-                        <li>{todo.content}</li>
-                        <li>{todo.date_created}</li>
-                        <li>todotodotoo</li>
-                    </ul>
+                    <table key={todo.id}>
+                        <tr>
+                            <th>Task</th>
+                            <th>Added</th>  
+                            <th>User</th>
+                            <th>Done</th>
+                            <th>Actions</th>
+                        </tr>
+                        <tr>
+                            <td>{todo.content}</td>
+                            <td>{todo.date_created}</td>
+                            <td>{todo.creator}</td>
+                            <td>{todo.completed}</td>
+                        </tr>
+                    </table>
                 )
             })}
         </div>
